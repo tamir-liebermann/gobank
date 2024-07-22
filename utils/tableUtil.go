c23f0b5f41c1f99cdce8f01a304fa5cc566202e8
tamir-liebermann <tamirlieb2@gmail.com>
2024-07-22 15:20:21 +0300

    more edits

--- utils/tableUtil.go
+++ utils/tableUtil.go
@@ -4,33 +4,48 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/olekukonko/tablewriter"
 )
 
 // getHeaders dynamically gets the headers from the JSON data
 func getSelectedHeaders() []string {
-	return []string{"from_account", "amount" , "to_account" , "timestamp"}
+	return []string{"transaction_details"}
 }
 
 // getSelectedRow returns only the selected columns from the record
-func getSelectedRow(record map[string]interface{}, myAccountId string) []string {
+func getSelectedRow(record map[string]interface{}, myAccountId string) ([]string, error) {
 	fromAccount := fmt.Sprintf("%v", record["from_account"])
 	toAccount := fmt.Sprintf("%v", record["to_account"])
 	amount := fmt.Sprintf("%v", record["amount"])
 	date := fmt.Sprintf("%v", record["timestamp"])
+	timestamp, err := time.Parse(time.RFC3339, date)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing timestamp: %v", err)
+	}
 
+	timeAgo := time.Since(timestamp)
+	var timeAgoStr string
+
+	switch {
+	case timeAgo.Hours() >= 24:
+		timeAgoStr = fmt.Sprintf("%v days ago", int(timeAgo.Hours()/24))
+	case timeAgo.Hours() >= 1:
+		timeAgoStr = fmt.Sprintf("%v hours ago", int(timeAgo.Hours()))
+	default:
+		timeAgoStr = fmt.Sprintf("%v minutes ago", int(timeAgo.Minutes()))
+	}
+	var details string
 	if fromAccount == myAccountId {
-		fromAccount = "your account"
-		amount = fmt.Sprintf("-%v", record["amount"])
+		details = fmt.Sprintf("your account -%v to %v %v ago", amount, toAccount, timeAgoStr)
+	} else if toAccount == myAccountId {
+		details = fmt.Sprintf("from %v %v your account %v ago", fromAccount, amount, timeAgoStr)
+	} else {
+		details = fmt.Sprintf("from %v %v to %v %v ago", fromAccount, amount, toAccount, timeAgoStr)
 	}
 
-	if toAccount == myAccountId {
-		toAccount = "your account"
-		amount = fmt.Sprintf("%v", record["amount"])
-	}
-
-	return []string{fromAccount, amount, toAccount, date}
+	return []string{details}, nil
 }
 
 
@@ -50,7 +65,11 @@ func FormatTransactionsTable(transactions interface{},myAccountId string) (strin
 	table := tablewriter.NewWriter(&buf)
 	table.SetHeader(getSelectedHeaders())
 	for _, record := range jsonData {
-		table.Append(getSelectedRow(record, myAccountId))
+		row, err := getSelectedRow(record, myAccountId)
+		if err != nil {
+			return "", fmt.Errorf("error formatting row: %v", err)
+		}
+		table.Append(row)
 	}
 	table.Render()
 
